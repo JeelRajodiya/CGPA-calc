c6a5c033210b6237e64eb72aac0a288a1fcc5138
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-05-14 22:33:05 +0530

    styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import "./App.css";
 import Subject from "./components/Subject/Subject";
+import Headings from "./components/Headings/Headings";
 export default function App() {
 	return (
 		<div className="container">
 			<h1 className="title">CGPA Calculator</h1>
+			<Headings />
 			<Subject name="Math" credit={3} grade={3.3} />
 			<Subject name="Math2" credit={3} grade={3.3} />
 		</div>
---
+++ src/components/Headings/Headings.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./Headings.css";
+
+export default function Heading() {
+	return (
+		<div className="headings">
+			<span>Subject</span>
+			<span>Credit</span>
+			<span>Grade</span>
+		</div>
+	);
+}
