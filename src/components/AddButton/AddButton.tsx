4068c9a4fef8b670bbe0c1e96ec487ce5e155110
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-05-15 16:41:36 +0530

    add button

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import "./AddButton.css";
-
+// @ts-ignore
+import AddIcon from "./add.png";
 export default function AddButton() {
 	return (
 		<div className="add-button bubble-element primary-button">
-			<span>Add Subject</span>
+			<img className="add-icon" src={AddIcon} alt="add icon" />
 		</div>
 	);
 }
